ad8c20331086961eea8f7fefa2c72ac42921fbaf
Abhay Dwivedi <abhaydwivedi557@gmail.com>
2026-09-19 02:00:10 +0530

    Time: 0 ms (100%), Space: 26.3 MB (8.53%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,12 +11,16 @@ public:
 
         while(st<=end){
             int mid = st + (end-st)/2 ;
-
+            
+            //check for the prev and next element wether is it same or not 
             if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]) return nums[mid];
 
+            //check for left half to be eliminated ( if mid odd then its prev would be same OR if it is even then its next would be same if satisfy then eliminate left half)
             if(mid%2==1 && nums[mid]==nums[mid-1] || mid%2==0 && nums[mid]==nums[mid+1]){
                 st = mid + 1;
             }
+
+            //if left half doesn't eliminate then the single element must be in left half so eliminate right half 
             else {
                 end = mid - 1;
             }
