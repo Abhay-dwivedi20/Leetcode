b08f1f6109ee3b7726e8aa3e67cc7fc30b4fde56
Abhay Dwivedi <abhaydwivedi557@gmail.com>
2026-09-23 01:56:24 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false ;
+        int org = x;
+        long long  rev = 0;
+         while (x != 0) {
+             int remainder = x % 10;
+              rev = rev * 10 + remainder;
+               x = x / 10;
+         }
+          return org == rev;
+           }
+};
